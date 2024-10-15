43256cccf62a683d64d900a1a30180270987e3b9
tuilakhanh <bacdau852@gmail.com>
2024-10-15 21:05:40 +0700

    feat: ec2 multi zone

--- 04-ec2.tf
+++ 04-ec2.tf
@@ -15,13 +15,15 @@ locals {
 module "public_ec2" {
   source = "./modules/ec2"
 
+  count = length(module.vpc.availability_zones)
+
   name          = "${var.project_name}-public-ec2"
   ami_id        = data.aws_ami.amazon_linux_23.id
   instance_type = local.instance_type
   key_name      = var.key_name
 
-  availability_zone  = element(module.vpc.availability_zones, 0)
-  subnet_id          = module.vpc.public_subnet_ids[0]
+  availability_zone  = element(module.vpc.availability_zones, count.index)
+  subnet_id          = element(module.vpc.public_subnet_ids, count.index)
   security_group_ids = [module.public_ec2_sg.security_group_id]
 
   associate_public_ip = true
@@ -30,13 +32,15 @@ module "public_ec2" {
 module "private_ec2" {
   source = "./modules/ec2"
 
+  count = length(module.vpc.availability_zones)
+
   name          = "${var.project_name}-private-ec2"
   ami_id        = data.aws_ami.amazon_linux_23.id
   instance_type = local.instance_type
   key_name      = var.key_name
 
-  availability_zone  = element(module.vpc.availability_zones, 1)
-  subnet_id          = module.vpc.private_subnet_ids[0]
+  availability_zone  = element(module.vpc.availability_zones, count.index)
+  subnet_id          = element(module.vpc.public_subnet_ids, count.index)
   security_group_ids = [module.private_ec2_sg.security_group_id]
 
   associate_public_ip = false
