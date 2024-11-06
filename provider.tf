a484fcee0e4a0bb6f8417d615995195c2a10b3c7
tiendat1011 <tiendat9tc@gmail.com>
2024-11-06 10:20:16 +0700

    feat: Add S3 Bucket to archive state, alter destroy workflow

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.aws_region
 }   
+
+terraform {
+backend "s3" {
+    bucket = "groupthreesix"
+    key    = "state/terraform.tfstate"  
+    region = var.aws_region  
+  }
+}
